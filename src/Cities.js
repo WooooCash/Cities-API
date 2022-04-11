b68f0eb45cc56fa84fcb5589227809c2c8546a58
WooooCash <lukaszbialczak3@gmail.com>
2022-04-11 21:27:06 +0200

    stupid validations smh

--- src/Cities.js
+++ src/Cities.js
@@ -5,18 +5,16 @@ import { getCities } from "./api";
 
 export default function Cities() {
 
-    const [cities, setCities] = useState({});
     const [currentInput, setCurrentInput] = useState('');
     const [finalQuery, setFinalQuery] = useState('');
     const [success, setSuccess] = useState(0);
 
     const checkInput = (e) => {
-        if (currentInput != '') {
-            setFinalQuery(currentInput);
-            getCities(currentInput).then(result => {
+        if (currentInput.trim != '') {
+            setFinalQuery(currentInput.trim);
+            getCities(currentInput.trim).then(result => {
                 let tempData = result.data;
-                setCities(tempData)
-                if (Object.values(tempData).map(el => el.name).includes(currentInput)) 
+                if (Object.values(tempData).map(el => el.name).includes(currentInput.trim)) 
                     setSuccess(1);
                 else
                     setSuccess(-1);
@@ -40,18 +38,6 @@ export default function Cities() {
             {success == -1 && (
                 <div className="fail">
                     <p>{finalQuery} doesn't exist...</p>
-                    {console.log("length", Object.keys(cities).length)}
-                    {Object.keys(cities).length > 0 && (
-                        <div>
-                            <hr />
-                            <p>did you mean...</p>
-                            <ul>
-                            {Object.values(cities).map(c => (
-                                <li>{c.name}</li>
-                            ))}
-                            </ul>
-                        </div>
-                    )}
                 </div>
             )}
         </div>
